8debdbc2c85c03fe842a26af441cf7a3433f2d3d
Ayush Jain <ayushjain2205@gmail.com>
2024-01-29 17:53:20 +0530

    Add geoloaction and nearby nft display

--- src/pages/hunts.jsx
+++ src/pages/hunts.jsx
@@ -1,10 +1,69 @@
 import React, { useEffect, useState } from "react";
-import GoogleMapReact from "google-map-react";
-
-const MapMarker = ({ text }) => <div>{text}</div>;
+import {
+  GoogleMap,
+  MarkerF,
+  LoadScript,
+  OverlayView,
+} from "@react-google-maps/api";
+import mapStyle from "../utils/mapStyle.json";
+import Stamp from "../components/UI/Stamp";
 
 const Hunts = () => {
   const [userLocation, setUserLocation] = useState(null);
+  const [randomPoints, setRandomPoints] = useState([]);
+
+  const containerStyle = {
+    width: "100%",
+    height: "100%",
+  };
+
+  const options = {
+    disableDefaultUI: true,
+    clickableIcons: false,
+    styles: mapStyle,
+  };
+
+  const getRandomCoordinates = (center, radius) => {
+    const getRandomNumber = (min, max) => Math.random() * (max - min) + min;
+
+    const earthRadius = 6371; // Radius of the Earth in kilometers
+    const randomDistance = getRandomNumber(0, radius / 1000); // Convert radius to kilometers
+    const randomBearing = getRandomNumber(0, 2 * Math.PI); // Bearing is in radians
+
+    const lat =
+      (Math.asin(
+        Math.sin((center.lat * Math.PI) / 180) *
+          Math.cos(randomDistance / earthRadius) +
+          Math.cos((center.lat * Math.PI) / 180) *
+            Math.sin(randomDistance / earthRadius) *
+            Math.cos(randomBearing)
+      ) *
+        180) /
+      Math.PI;
+
+    const lng =
+      center.lng +
+      (Math.atan2(
+        Math.sin(randomBearing) *
+          Math.sin(randomDistance / earthRadius) *
+          Math.cos((center.lat * Math.PI) / 180),
+        Math.cos(randomDistance / earthRadius) -
+          Math.sin((center.lat * Math.PI) / 180) *
+            Math.sin((lat * Math.PI) / 180)
+      ) *
+        180) /
+        Math.PI;
+
+    return { lat, lng };
+  };
+
+  const stampData = [
+    { image: "/images/mickey.png", color: "#FFC022" },
+    { image: "/images/hm.png", color: "#FB4912" },
+    { image: "/images/apple.png", color: "#FCFCFC" },
+    { image: "/images/mcd.png", color: "#FEC7C7" },
+    { image: "/images/starbucks.png", color: "#F091F9" },
+  ];
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -12,6 +71,15 @@ const Hunts = () => {
         (position) => {
           const { latitude, longitude } = position.coords;
           setUserLocation({ lat: latitude, lng: longitude });
+
+          // Generate 5 random points and update the state
+          const points = [];
+          for (let i = 0; i < 5; i++) {
+            points.push(
+              getRandomCoordinates({ lat: latitude, lng: longitude }, 3000)
+            );
+          }
+          setRandomPoints(points);
         },
         (error) => {
           console.error("Error getting user location:", error);
@@ -23,21 +91,40 @@ const Hunts = () => {
   }, []);
 
   return (
-    <div style={{ height: "100vh", width: "100%" }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY }}
-        defaultCenter={{ lat: 0, lng: 0 }} // Default center
-        defaultZoom={12}
-        center={userLocation || { lat: 0, lng: 0 }} // Center map at user's location if available
+    <div className="relative" style={{ height: "665px", width: "390px" }}>
+      <div className="absolute flex flex-col justify-center px-[20px] top-0 left-0 h-[52px] w-full z-10 bg-[#FFC022] ">
+        <span className="text-[28px]"> EXPLORE..</span>
+      </div>
+      <LoadScript
+        googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
       >
-        {userLocation && (
-          <MapMarker
-            lat={userLocation.lat}
-            lng={userLocation.lng}
-            text="Your Location"
-          />
-        )}
-      </GoogleMapReact>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={userLocation || { lat: 0, lng: 0 }}
+          zoom={15}
+          options={options}
+        >
+          {userLocation && (
+            <MarkerF
+              icon="/images/marker.png"
+              position={{ lat: userLocation.lat, lng: userLocation.lng }}
+            />
+          )}
+          {randomPoints.map((point, index) => (
+            <OverlayView
+              key={index}
+              mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+              position={{ lat: point.lat, lng: point.lng }}
+            >
+              <Stamp
+                image={stampData[index].image}
+                color={stampData[index].color}
+                marker
+              />
+            </OverlayView>
+          ))}
+        </GoogleMap>
+      </LoadScript>
     </div>
   );
 };
