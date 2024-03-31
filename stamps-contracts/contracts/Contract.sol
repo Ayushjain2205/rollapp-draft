82c0c9362550ec4429e67a0d7a07e0b4ce5caf82
Ayush Jain <ayushjain2205@gmail.com>
2024-03-31 18:53:05 +0530

    Update contract

--- stamps-contracts/contracts/Contract.sol
+++ stamps-contracts/contracts/Contract.sol
@@ -1,6 +1,75 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
-contract MyContract {
-    constructor() {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+
+contract Stamps is
+    ERC721,
+    ERC721Enumerable,
+    ERC721Pausable,
+    Ownable,
+    ERC721Burnable
+{
+    uint256 private _nextTokenId;
+
+    struct Geotag {
+        int256 latitude;
+        int256 longitude;
+    }
+
+    mapping(uint256 => Geotag) private _tokenGeotags;
+
+    constructor() ERC721("Stamps", "STAMP") {
+        transferOwnership(msg.sender); // Set the owner to the deployer or specific address if needed
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(
+        address to,
+        int256 latitude,
+        int256 longitude
+    ) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+
+        _tokenGeotags[tokenId] = Geotag(latitude, longitude);
+    }
+
+    function getGeotag(uint256 tokenId) public view returns (int256, int256) {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: Query for nonexistent token"
+        );
+        return (
+            _tokenGeotags[tokenId].latitude,
+            _tokenGeotags[tokenId].longitude
+        );
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    // Corrected override for _beforeTokenTransfer
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 amount
+    ) internal override(ERC721, ERC721Enumerable, ERC721Pausable) {
+        super._beforeTokenTransfer(from, to, tokenId, amount);
+    }
 }
--- stamps-contracts/hardhat.config.js
+++ stamps-contracts/hardhat.config.js
@@ -12,17 +12,10 @@ module.exports = {
     },
   },
   networks: {
-    zksync_testnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli",
-      chainId: 280,
-      zksync: true,
-    },
-    zksync_mainnet: {
-      url: "https://zksync2-mainnet.zksync.io/",
-      ethNetwork: "mainnet",
-      chainId: 324,
-      zksync: true,
+    botanix_testnet: {
+      url: "https://3636.rpc.thirdweb.com",
+      ethNetwork: "botanix_testnet",
+      chainId: 3636,
     },
   },
   paths: {
