30bb5302487a2688ddd41365df387a7f62affbca
Ayush Jain <ayushjain2205@gmail.com>
2024-03-31 12:53:08 +0530

    Default to botanix testnet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThirdwebProvider, embeddedWallet } from "@thirdweb-dev/react";
+import { BotanixTestnet } from "@thirdweb-dev/chains";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      activeChain="goerli"
+      activeChain={BotanixTestnet}
       clientId="a847530a1045ffa564dad14048937e5c"
       supportedWallets={[embeddedWallet()]}
     >
